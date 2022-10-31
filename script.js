0b231c57cd11736c60f9230901c5dcfcaba3076e
John Siciliano <johnsiciliano@Johns-iMac.local>
2022-10-31 11:34:22 -0700

    add main script

---
+++ script.js
@@ -0,0 +1,5 @@
+(function() {
+    if (!CallTrkSwap._referrer.includes('attorneyatlawmagazine.com')) {
+        document.querySelector('form').setAttribute('data-cr-no-capture', 'true');
+    }
+})();
