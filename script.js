b0ba059653adf886403cc349890802c191e2bd48
John Siciliano <johnsiciliano@Johns-iMac.local>
2022-11-02 14:42:28 -0700

    add window load event listener

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-(function() {
-    if (!CallTrkSwap._referrer.includes('attorneyatlawmagazine.com')) {
-        document.querySelector('form').setAttribute('data-cr-no-capture', 'true');
-    }
-})();
+window.addEventListener('load', function() {
+  if (!CallTrkSwap._referrer.includes('attorneyatlawmagazine.com')) {
+      document.querySelector('form').setAttribute('data-cr-no-capture', 'true');
+  }
+});
