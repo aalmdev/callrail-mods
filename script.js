6390d6dc916ca5c38a3c3f301971a2a2ad56ec0a
johnsicili <93934362+johnsicili@users.noreply.github.com>
2022-12-15 10:12:13 -0700

    Fix error and loop through forms
    Moved attribute removal to new line as set attribute returns undefined.. chaining doesn't work. Also loop through forms incase there are multiple forms on a page.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function() {
   if (!CallTrkSwap._referrer.includes('attorneyatlawmagazine.com')) {
-      document.querySelector('form').setAttribute('data-cr-no-capture', 'true').removeAttribute('cr-attached');
+      const forms = document.querySelectorAll('form');
+      forms.forEach((form) => {
+        form.setAttribute('data-cr-no-capture', 'true');
+        form.removeAttribute('cr-attached');
+      });
   }
 });
